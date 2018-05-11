377f09f8276af4fa2936c0771521d9a7738d4cc2
emilyporat <eporat@andrew.cmu.edu>
2018-05-11 00:40:17 -0400

    mobile styles plus newstagram page

---
+++ js/general.js
@@ -0,0 +1,53 @@
+var myNav = document.getElementById('my-nav');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 200 ) {
+    	myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    } 
+    else {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    }
+};
+
+function slideDown() {
+	var h = Math.max(document.documentElement.clientHeight);
+	window.scroll({
+	  top: h, 
+	  left: 0, 
+	  behavior: 'smooth' 
+	});	
+}
+
+function slideUp() {
+	window.scroll({
+	  top: 0, 
+	  left: 0, 
+	  behavior: 'smooth' 
+	});
+}
+
+function slideToSection(sectionId) {
+	var section = document.getElementById(sectionId).getBoundingClientRect();
+	window.scroll({
+	  top: section.top + window.pageYOffset - 50, 
+	  left: 0, 
+	  behavior: 'smooth' 
+	});	
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
