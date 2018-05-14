e4d5449f5b6cdb825bf84a11341efe738504a780
emilyporat <eporat@andrew.cmu.edu>
2018-05-14 17:08:03 -0400

    fixed project styles, deleted fave classes

--- js/general.js
+++ js/general.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
+            if( bottom_of_window > (bottom_of_object - 250) ){
                 
                 $(this).animate({'opacity':'1'},500);
                     
