e17abcb7b4d36f8683fff328a3bd20b0f10d4b48
Emily Porat <emporat@gmail.com>
2020-04-20 13:48:07 -0400

    updated footer

--- js/general.js
+++ js/general.js
@@ -150,6 +150,30 @@ $(document).ready(function() {
    
 });
 
+$(document).ready(function() {
+   
+   /* Every time the window is scrolled ... */
+   $(window).scroll( function(){
+   
+       /* Check the location of each desired element */
+       $('.section').each( function(i){
+           
+           var top_of_object = $(this).position().top;
+           var bottom_of_window = $(window).scrollTop() + $(window).height();
+           
+           /* If the object is completely visible in the window, fade it it */
+           if( bottom_of_window > top_of_object ){
+               
+               $(this).animate({'opacity':'1'},700);
+                   
+           }
+           
+       }); 
+   
+   });
+   
+});
+
 $(document).ready(function () {
     $('.big-pic').animate({'opacity':'1'},900);
     $('.title').animate({'opacity':'1'},900);
